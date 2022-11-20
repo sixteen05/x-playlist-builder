8d436e60194f26dfaa471e41bdcf0eb095cd92b7
Apoorv Vardhan <vardhanapoorv@gmail.com>
2022-11-20 11:36:10 +0530

    Add auth impl for spotify client

--- src/auth.rs
+++ src/auth.rs
@@ -1,21 +1,30 @@
-use rspotify::{
-    scopes, AuthCodeSpotify, Credentials, OAuth,
-};
+use rspotify::{scopes, AuthCodeSpotify, Credentials, OAuth, prelude::*,};
 
-pub fn init_spotify() -> AuthCodeSpotify {
-    let oauth = OAuth {
-        scopes: scopes!(
-            "user-read-private",
-            "user-read-email",
-            "user-library-read",
-            "user-library-modify",
-            "playlist-modify-private",
-            "playlist-read-private"
-        ),
-        redirect_uri: "http://localhost:8080/callback".to_owned(),
-        ..Default::default()
-    };
+pub struct SpotifyAuth {
+    pub client: AuthCodeSpotify,
+}
 
-    let creds = Credentials::from_env();
-    AuthCodeSpotify::new(creds.unwrap(), oauth)
+impl SpotifyAuth {
+    pub async fn new() -> Self {
+        let oauth = OAuth {
+            scopes: scopes!(
+                "user-read-private",
+                "user-read-email",
+                "user-library-read",
+                "user-library-modify",
+                "playlist-modify-private",
+                "playlist-read-private"
+            ),
+            redirect_uri: "http://localhost:8080/callback".to_owned(),
+            ..Default::default()
+        };
+
+        let creds = Credentials::from_env();
+        let mut client = AuthCodeSpotify::new(creds.unwrap(), oauth);
+        let url = client.get_authorize_url(false).unwrap();
+        client.prompt_for_token(&url).await.unwrap();
+        Self {
+            client: client,
+        }
+    }
 }
