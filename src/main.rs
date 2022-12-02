7c84ae9482da997f8b07a6305ee08dd3f23d47c6
Apoorv Vardhan <vardhanapoorv@gmail.com>
2022-12-02 20:13:07 +0530

    Get condition name & value from endpoint. Support for artist added :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use rspotify::{
     model::{PlayableId, TrackId},
     prelude::*,
 };
+use serde::Deserialize;
 use x_playlist_builder::{
     auth::SpotifyAuth,
-    playlist::{create_or_get_playlist, get_all_playlist_created_by_user}, util::fetch_all,
-    filter:: filter_by_condition,
+    filter::{filter_by_condition, filter_condition_to_playlist_name},
+    playlist::{create_or_get_playlist, get_all_playlist_created_by_user},
+    util::fetch_all,
 };
 
 #[get("/me/playlists")]
@@ -19,26 +21,35 @@ async fn get_all_playlists() -> impl Responder {
     return HttpResponse::Ok().body("Got all user playlists!");
 }
 
-#[get("/liked/old-songs/create-update-playlist")]
-async fn liked_songs() -> impl Responder {
+#[derive(Deserialize)]
+struct Condition {
+    name: String,
+    value: String,
+}
+
+#[get("/liked/create-update-playlist/condition/{name}/{value}")]
+async fn liked_songs(info: web::Path<Condition>) -> impl Responder {
+    let condition_name = &info.name;
+    let condition_value = &info.value;
     let resp = SpotifyAuth::new().await;
     let spotify = resp.client;
-    let fullplaylist = create_or_get_playlist(&spotify).await;
+    let playlist_name = filter_condition_to_playlist_name(condition_name, condition_value);
+    let existing_playlist = create_or_get_playlist(&spotify, playlist_name).await;
 
     let current_user_saved_tracks = fetch_all(spotify.current_user_saved_tracks(None)).await;
     let mut tracks: Vec<TrackId> = Vec::new();
 
-    for item in current_user_saved_tracks{
-        let r = item.track.album.release_date.as_ref();
-        let release_year = r.unwrap().split("-").next().unwrap();
-        let year_val = release_year.parse::<i32>().unwrap();
-        if filter_by_condition("old-hindi".to_string(), year_val, item.track.available_markets)
-        {
-            println!("* {}, Year - {}", item.track.name, release_year);
-            let trackid = item.track.id.unwrap();
+    for item in current_user_saved_tracks {
+        let filter_res = filter_by_condition(
+            condition_name,
+            condition_value,
+            item.track
+        );
+        if filter_res.state {
+            let trackid = filter_res.track_id;
             let mut song_already_exists = false;
-            for item in &fullplaylist.tracks.items {
-                if item.track.as_ref().unwrap().id().unwrap().id() == trackid.id() {
+            for playlist_item in &existing_playlist.tracks.items {
+                if playlist_item.track.as_ref().unwrap().id().unwrap().id() == trackid.id() {
                     song_already_exists = true;
                     break;
                 }
@@ -52,7 +63,7 @@ async fn liked_songs() -> impl Responder {
     if tracks.len() != 0 {
         spotify
             .playlist_add_items(
-                &fullplaylist.id,
+                &existing_playlist.id,
                 tracks.iter().map(|track| track as &dyn PlayableId),
                 None,
             )
@@ -61,7 +72,7 @@ async fn liked_songs() -> impl Responder {
     }
 
     return HttpResponse::Ok()
-        .body("Got liked songs & created/updated old songs playlist by x-playlist-builder!");
+        .body("Got liked songs & created/updated playlist with songs!");
 }
 
 #[actix_web::main]
